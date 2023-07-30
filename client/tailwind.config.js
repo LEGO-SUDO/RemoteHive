45e98845dcaf0be8f81bb7b92c3d3659beb4e199
NAKUL  SINGH <nakulsingh@NAKULs-MacBook-Air.local>
2023-07-30 22:03:48 +0530

    signup form validation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: { sm: "375px", md: "834px", lg: "1440px" },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
